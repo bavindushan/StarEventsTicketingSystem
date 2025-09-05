4dcfdfefec589cdf7331b6654b33338a51db23d8
bavindushan <bavindushan65@gmail.com>
2025-09-05 19:04:57 +0530

    static home page created with hardcode data

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StarEventsTicketingSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // For now, we can pass placeholder data
+            return View();
+        }
+    }
+}
